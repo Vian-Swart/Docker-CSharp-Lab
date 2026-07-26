9af0249e2ab15de69b60728d749bb1245f7cc057
Vian Swart <vian@snappycleaning.co.za>
2026-07-26 19:44:18 +0000

    Initial Commit

---
+++ HelloDocker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloDocker
+{
+class Program
+{
+static void Main(string[] args)
+{
+Console.WriteLine("=================================="); 
+Console.WriteLine("Welcome to GitHub Codespaces"); 
+Console.WriteLine("Learning Docker with C#"); 
+Console.WriteLine("==================================");
+
+Console.Write("Enter your name: "); 
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hello {name}!"); 
+Console.WriteLine("I am ready to learn Docker!");
+}
+}
+}
